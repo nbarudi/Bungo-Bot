320201970ef02554b5eb542a8688fd5f2be5d99e
nbarudi <nbarudi@outlook.com>
2018-01-14 17:07:43 -0500

    Update unban.js

--- cmds/unban.js
+++ cmds/unban.js
@@ -1,16 +1,20 @@
 const Discord = module.require("discord.js")
-const fs = require("fs")
 
 module.exports.run = async (bot, message, args) => {
     var hours = (new Date()).getHours();
-        if(!message.member.roles.has("390535734093807636")) return message.channel.send("Sorry, you do not have the correct permissions!")
+    let adminRole = message.guild.roles.find("name", "Moderator")
+    if(!message.member.roles.has(adminRole.id)) return message.channel.send("Sorry, you do not have the correct permissions!")
 
         let toKick = message.guild.members.get(args[0])
         var person = message.guild.member(message.author)
 
         message.guild.member(toKick).ban("You have been removed! If this is a mistake then send a message to the owner of  the discord!")
 
-        message.channel.send(`Banned ${toKick}!`).then(message => message.delete(5000))
+        message.channel.send(`UnBanned ${toKick}!`).then(message => message.delete(5000))
+        for(let i in bot.logs) {
+            if (!bot.logs[i] == message.guild.id) continue
+            let templogs = bot.logs[i].logs_channel
+            let logs = message.guild.channels.find("id",templogs)
         let logs = bot.channels.get("390535128322932746")
         embed = new Discord.RichEmbed()
         embed.setTitle(`Unbanning!`)
@@ -19,6 +23,7 @@ module.exports.run = async (bot, message, args) => {
         embed.addField(`Ran At: `, `${hours} hours into the day`)
         embed.setColor(`#ff0000`)
         logs.send({embed: embed})
+        }
 
         if (message.guild.bans.has(toKick)){
             console.log("Player is banned!")
