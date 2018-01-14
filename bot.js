5a493c0826aafbbd47caffebe60a8743390e2f16
nbarudi <nbarudi@outlook.com>
2018-01-14 14:30:54 -0500

    The main bot systems
    Here is the main script of the bot

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+
+const bot = new Discord.Client()
+
+const prefix = process.env.BOT_PREFIX
+bot.commands = new Discord.Collection()
+
+fs.readdir("./cmds", (err, files) =>{
+    if (err) console.error(err)
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js")
+    if (jsfiles.length <= 0) {
+        console.log("No Commands To Load!")
+        return
+    }
+
+    console.log(`Loading ${jsfiles.length} commands!`)
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./cmds/${f}`)
+        console.log(`${i = 1}: ${f} Loaded!`)
+        bot.commands.set(props.help.name, props)
+
+    })
+
+})
+
+bot.on("ready", () =>{
+    console.log("Bot is now online!")
+    console.log('The current prefix is: ' + botSettings.prefix)
+    console.log("Running on the name: " + bot.user.username)
+    console.log('The current id is: ' + bot.user.id)
+})
+
+bot.on("message", async message =>{
+    if(message.author.bot) return
+    if(message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ")
+    let command = messageArray[0]
+    let args = messageArray.slice(1);
+
+    if(!message.content.startsWith(prefix)) return;
+
+    let cmd = bot.commands.get(command.slice(prefix.length))
+    if(cmd) cmd.run(bot, message, args)
+})
+
+bot.login(process.env.BOT_TOKEN)
